c68363ebb7f0966b422203bef0ffdf1b6c59e99a
nathan-stamand <nathansaint810@gmail.com>
2020-07-29 11:44:47 -0500

    created seed data for projects and steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+proj_1 = Project.create(name: 'Gnome Alone', blog: 'Quick intro to making your own gnomes!', materials: 'pieces of a broken gnome, glue', total_time: 64)
+proj_2 = Project.create(name: 'Pecan Air: Starring Nick Cage', blog: 'Procedure for making a plane out of a pecan!', materials: 'an old pecan (shelled), cardstock, gluestick', total_time: 38)
+
+step_1 = proj_1.steps.create(header: 'step 1: glue gnome together', materials: 'pieces of a broken gnome, glue', time: 64, directions: "{1: 'just drench that thing in glue'}")
+step_2 = proj_2.steps.create(header: 'Get Off The Ground', materials: 'an old pecan (shelled)', time: 15, directions: "{1: 'clean your pecan with warm water', 2: 'allow to dry for 10 min'}")
+step_3 = proj_2.steps.create(header: 'Take Flight!', materials: 'cardstock', time: 15, directions: "{1: 'cut or rip the paper into something that looks like plane wings'}")
+step_4 = proj_2.steps.create(header: 'Take That, Gravity!', materials: 'an old pecan (shelled), cardstock, glue', time: 8, directions: "{1: 'glue those bad boys to your clean pecan and throw it around!'}")
