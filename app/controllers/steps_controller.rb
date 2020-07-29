8d21391ac9867e2a4ce1c6c23f6ba3d7f300606d
nathan-stamand <nathansaint810@gmail.com>
2020-07-29 11:51:57 -0500

    added #index

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,2 +1,7 @@
 class StepsController < ApplicationController
+  def index
+    project = Project.find_by(id: params[:project_id])
+    steps = project.steps
+    render json: StepSerializer.new(steps)
+  end
 end
