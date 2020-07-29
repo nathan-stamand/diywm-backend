2c0b91174d7afb9c235c95ff3381248e07d8fb35
nathan-stamand <nathansaint810@gmail.com>
2020-07-29 13:44:54 -0500

    added #show with conditionals for if project/step is not found

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,4 +4,14 @@ class StepsController < ApplicationController
     steps = project.steps
     render json: StepSerializer.new(steps)
   end
+
+  def show
+    project = Project.find_by(id: params[:project_id])
+    project ? step = project.steps.select(|step| step.id == params[:id]) : step = nil
+    if step
+      render json: StepSerializer.new(step)
+    else
+      render json: {message: 'No step found. Please refresh and try again.'}
+    end
+  end
 end
