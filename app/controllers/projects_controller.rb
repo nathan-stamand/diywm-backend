8ac9e31645f28a24d57a8185fa87fd3e59826165
nathan-stamand <nathansaint810@gmail.com>
2020-07-29 12:04:02 -0500

    added #create w/ conditional to render the created project if valid or render error message if not

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,4 +8,19 @@ class ProjectsController < ApplicationController
     project = Project.find_by(id: params[:id])
     render json: ProjectSerializer.new(project)
   end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      render json: ProjectSerializer.new(project)
+    else
+      errorMsg = 
+      <<-MSG
+      Failed to create project. 
+      Please double check that you have input valid information. 
+      MSG
+      render json: {
+          message: errorMsg
+        }
+  end
 end
