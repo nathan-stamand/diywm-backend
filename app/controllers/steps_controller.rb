ddb06e519e22d87c94ee9c8220c9c9bbd76f69fd
nathan-stamand <nathansaint810@gmail.com>
2020-07-29 13:57:12 -0500

    added #create with conditionals for invalid step params or project not found

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -14,4 +14,20 @@ class StepsController < ApplicationController
       render json: {message: 'No step found. Please refresh and try again.'}
     end
   end
+
+  def create
+    project = Project.find_by(id: params[:project_id])
+    if project
+      step = project.steps.build(step_params)
+      if step.save
+        render json: StepSerializer.new(step)
+      else
+        render json: { message: 'Step could not be created. Please make sure inputs are valid.' }
+      end
+    else
+      render json: {
+        message: 'Project not found: Step could not be created. Please refresh and try again.'
+      }
+    end
+  end
 end
